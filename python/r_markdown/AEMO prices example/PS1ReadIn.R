b632cf40788de97fc9af7f9cbfd5b833ac933b94
Oliver Sheridan-Methven <oliver.sheridan-methven@maths.ox.ac.uk>
2019-05-15 18:16:40 +0100

    Removed redundant file.

--- python/r_markdown/AEMO prices example/PS1ReadIn.R
+++
@@ -1,9 +0,0 @@
-AEMO<-read.csv("AEMOPriceDataSimple.csv")
-SydTemp<-read.csv("SydTemp.csv")
-minTemp<-SydTemp$Minimum.temperature..Degree.C.
-maxTemp<-SydTemp$Maximum.temperature..Degree.C.
-maxTemp<-maxTemp[SydTemp$Year==2013]
-minTemp<-minTemp[SydTemp$Year==2013]
-maxTemp<-maxTemp[!is.na(maxTemp)]
-minTemp<-minTemp[!is.na(minTemp)]
-Date<-as.Date(AEMO$Date, '%d/%m/%y')
